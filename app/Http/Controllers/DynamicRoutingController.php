968405eb1cb2225503e0ca2c061b45e998e46c0a
Myke <myke.agonia@gmail.com>
2023-03-22 10:57:59 +0800

    Initial commit

---
+++ app/Http/Controllers/DynamicRoutingController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DynamicRoutingController extends Controller
+{
+    public function handleGetRequest(Request $request)
+    {
+        $csrfToken = csrf_token(); //manually copy this value in POST Request header X-CSRF-TOKEN
+        $url = $request->url();
+        $host = $request->getHttpHost(); //or getHost();
+        $agent = $request->header('User-Agent'); //or $request->userAgent();
+        $response = response()->json([
+                    'csrfToken' => $csrfToken,
+                    'url' => $url,
+                    'agent string' => $agent,
+                    'host' => $host
+                ]);
+        return $response;
+    }
+
+    public function handlePostRequest(Request $request)
+    {
+        $data = $request->all();
+        $url = $request->url();
+        $host = $request->getHttpHost(); //or getHost();
+        $agent = $request->header('User-Agent'); //or $request->userAgent();
+        $response = response()->json([
+                        'url' => $url,
+                        'agent string' => $agent,
+                        'host' => $host,
+                        'body' => $data
+                    ]);
+        return $response;
+    }
+}
